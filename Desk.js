e7459ad6895c3c454c712b4e9b49da387de1fd25
PRATYASHASAMALtwozerooneone <111174419+PRATYASHASAMALtwozerooneone@users.noreply.github.com>
2024-05-29 18:12:45 +0530

    Add files via upload

---
+++ Desk.js
@@ -0,0 +1,46 @@
+status1 = "";
+img = "";
+objects = [];
+
+
+function preload(){
+    img = loadImage('Desk.webp');
+}
+
+function setup() {
+    canvas = createCanvas(640, 350);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    
+}
+
+function draw() {
+    if (status1 != undefined) {
+          image(img, 0, 0, 640, 420);
+      for (var i = 0; i < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+  
+        fill(255, 0, 0);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+        noFill();
+        stroke(255, 0, 0);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
+  }
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
